f872e11ae835d7e8a61e4356d8d4119ec7411cec
Simone PriMaL Maccarone <maccaronesimone03@gmail.com>
2023-07-20 18:44:16 +0200

    fix css

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { RecipeService } from 'src/app/services/recipeServ/recipe.service';
 
 @Component({
