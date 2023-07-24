05eabeeb37c848b936b87f135402f137f6587fb9
Simone PriMaL Maccarone <maccaronesimone03@gmail.com>
2023-07-24 12:47:41 +0200

    return home animation

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Hit, RecipeModel } from 'src/app/interfaces/recipe-model';
 import { RecipeService } from 'src/app/services/recipeServ/recipe.service';
 
@@ -11,6 +11,8 @@ export class RecipeComponent implements OnInit {
   searchQuery: string = ""; // Inizializzazione durante la dichiarazione
   searchResults: Hit[] = []; // da specificare (es: RootRecipe[])
   linkNextPage: any;
+
+  showBackToTop = false;
 
   constructor(private recipeService: RecipeService) { this.onSubmit(); }
   ngOnInit(): void {
@@ -27,6 +29,29 @@ export class RecipeComponent implements OnInit {
     );
   }
 
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.showBackToTop = (window.pageYOffset > 200); // Mostra il pulsante solo dopo uno scroll di 200px
+  }
+
+  scrollToTop(): void {
+    // Questa funzione farà lo scroll verso l'alto con un'animazione
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 200); // Controlla la velocità dello scroll regolando il valore "-20"
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 8);
+  }
+
+
+
+
+
+
   //NEXT PAGE FOOD
   // getNextPage() {
   //   this.recipeService.searchRecipes(this.searchQuery).subscribe(
@@ -36,9 +61,5 @@ export class RecipeComponent implements OnInit {
   //     }
   //   )
   // }
-
-
-
-
 }
 
