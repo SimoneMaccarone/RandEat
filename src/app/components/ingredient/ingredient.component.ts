a5ae592713e1750a5b28b8a7c56e3ac13802de41
Simone PriMaL Maccarone <maccaronesimone03@gmail.com>
2023-07-26 13:09:00 +0200

    INGR fix but doesnt work

--- src/app/components/ingredient/ingredient.component.ts
+++ src/app/components/ingredient/ingredient.component.ts
@@ -8,7 +8,7 @@ import { RecipeService } from 'src/app/services/recipeServ/recipe.service';
   styleUrls: ['./ingredient.component.scss']
 })
 export class IngredientComponent  {
-  recipes: Hit[]=[];
+  recipes: any[]=[];
   ingredients: string='';
 
   showBackToTop = false;
@@ -17,16 +17,34 @@ export class IngredientComponent  {
 
   constructor(private recipeService: RecipeService){ }
 
-  searchRecipes(ingredients:string[]) {
-    this.recipeService.searchRecipesByIngredients(ingredients).subscribe(
+  searchRecipes(ingredients:string):any {
+    const ingredientsArray = ingredients.split(',');
+    this.recipeService.searchRecipesByIngredients(ingredientsArray).subscribe(
       {
-        next: recipes => this.recipes = recipes.hits,
+        next: recipes => this.recipes = this.filterRecipesByIngredients(recipes.hits, ingredientsArray),
         error: err => console.log('Errore nella ricerca Ingredienti', err)
       }
     );
   }
 
+  filterRecipesByIngredients(recipes: any[], ingredients: string[]): any[] {
+    // Confronta gli ingredienti di ciascuna ricetta con l'elenco degli ingredienti cercati
+    return recipes.filter(recipe => this.containsAllIngredients(recipe.recipe.ingredientLines, ingredients));
+  }
 
+  containsAllIngredients(ingredientLines: string[], ingredients: string[]): boolean {
+    // Verifica se tutti gli ingredienti cercati sono presenti nelle righe degli ingredienti della ricetta
+    const lowerCaseIngredients = ingredients.map(ingredient => ingredient.toLowerCase().trim());
+    const lowerCaseIngredientLines = ingredientLines.map(line => line.toLowerCase());
+    return lowerCaseIngredients.every(ingredient => lowerCaseIngredientLines.some(line => line.includes(ingredient)));
+  }
+
+
+
+
+
+
+  //------------------------------------------------------------------------
   @HostListener('window:scroll', [])
   onScroll(): void {
     this.showBackToTop = (window.scrollY > this.scrollOffsetToShowButton);
--- src/app/services/recipeServ/recipe.service.ts
+++ src/app/services/recipeServ/recipe.service.ts
@@ -13,36 +13,42 @@ export class RecipeService {
   APP_KEY: string = '7b2258d83d315207e9cc144eb81ef82b';
   baseURL: string = 'https://api.edamam.com/api/recipes/v2'
 
+
   // INGREDIENTS RECIPE EDAMAM API
-  // https://api.edamam.com/search?q=eggs,bacon&app_id=746aee35&app_key=7b2258d83d315207e9cc144eb81ef82b
-  APP_ID_INGR: string = 'b9d26be2'
-  APP_KEY_INGR: string = '1250ba1410896310c611d2fa2659ff72'
-  baseURL_INGR: string = 'https://api.edamam.com/api/food-database/v2/parser'
-
-  baseURL_RANDOM: string = 'https://www.themealdb.com/api/json/v1/1/random.php'
-
   // https://api.edamam.com/api/food-database/v2/parser?ingr={ingredient}&app_id=b9d26be2&app_key=1250ba1410896310c611d2fa2659ff72
+  // https://api.edamam.com/search?q=eggs,bacon&app_id=746aee35&app_key=7b2258d83d315207e9cc144eb81ef82b
 
+  // APP_ID_INGR: string = 'b9d26be2'
+  // APP_KEY_INGR: string = '1250ba1410896310c611d2fa2659ff72'
+  // baseURL_INGR: string = 'https://api.edamam.com/api/food-database/v2/parser'
+
+  APP_ID_INGR: string = 'eeaee5a6'
+  APP_KEY_INGR: string = 'a95083252ece72edbfc4f7aeb9dc6a33'
+  baseURL_INGR : string= 'https://api.edamam.com/search'
+
+
+  // RANDOM RECIPE API
+  baseURL_RANDOM: string = 'https://www.themealdb.com/api/json/v1/1/random.php'
   // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
   //🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕
   // ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 
   constructor(private http: HttpClient) { }
 
-  // SEARCH RECIPE
+  // SEARCH RECIPE        //baseURL: string = 'https://api.edamam.com/api/recipes/v2'
   searchRecipes(query: string): Observable<RecipeModel> {
     const url = `${this.baseURL}?type=public&q=${query}&app_id=${this.APP_ID}&app_key=${this.APP_KEY}&from=0&to=20`;
     return this.http.get<any>(url);
   }
 
   // INGREDIENTS RECIPE
-  searchRecipesByIngredients(ingredients: string[]): Observable<IngredientsModel> {
+  searchRecipesByIngredients(ingredients: string[]): Observable<any> {
     const joinedIngredients = ingredients.join(',');
     const url=`${this.baseURL_INGR}?q=${joinedIngredients}&app_id=${this.APP_ID_INGR}&app_key=${this.APP_KEY_INGR}`;
     return this.http.get<any>(url);
   }
 
-  // RANDOM RECIPE
+  // RANDOM RECIPE        //baseURL_RANDOM: string = 'https://www.themealdb.com/api/json/v1/1/random.php'
   getRandomRecipe(): Observable<RandomRecipeModel[]> {
     return this.http.get<any>(this.baseURL_RANDOM).pipe(
       switchMap(randomRecipe => {
