96ca754cd182bda73670b996b4668773129b9881
Simone PriMaL Maccarone <maccaronesimone03@gmail.com>
2023-09-01 13:34:04 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { RandomComponent } from './components/random/random.component';
 import { IngredientComponent } from './components/ingredient/ingredient.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'search', component: RecipeComponent },
   { path: 'ingredient', component: IngredientComponent },
   { path: 'random', component: RandomComponent },
-  // { path: '', component: RecipeComponent },
-  // { path: '**', component: RecipeComponent },// wrong search
+  { path: '',   redirectTo: '', pathMatch: 'full' },
+  // { path: '**',   redirectTo: '/search', pathMatch: 'full' },
+
+  // { path: '', component: AppComponent },
+  // { path: '**', component:  },// wrong search
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // UseHash: true
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
